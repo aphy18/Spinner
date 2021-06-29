b41ff9b80638cb0dde6c162c4ee6dc8f39452d65
aphy18 <aphason1@.com>
2021-06-29 19:39:29 +0000

    spinning animation

---
+++ spinner1.js
@@ -0,0 +1,39 @@
+    setTimeout(() => {
+    process.stdout.write('\r|');
+  }, 500);
+  
+  setTimeout(() => {
+    process.stdout.write('\r/');
+  }, 1000);
+  
+  setTimeout(() => {
+    process.stdout.write('\r-');
+  }, 1500);
+  
+  setTimeout(() => {
+    process.stdout.write('\r\\'); 
+  }, 2000);
+
+  setTimeout(() => {
+      process.stdout.write('\r|')
+  },2500)
+
+  setTimeout(() => {
+    process.stdout.write('\r/')
+},3000)
+
+setTimeout(() => {
+    process.stdout.write('\r-')
+},3500)
+
+setTimeout(() => {
+    process.stdout.write('\r\\')
+},4000)
+
+setTimeout(() => {
+    process.stdout.write('\r|')
+},4500)
+
+setTimeout(() => {
+    process.stdout.write("\n");
+},5000)
